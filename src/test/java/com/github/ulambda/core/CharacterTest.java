663e18eadcc435f646229cedaba8dd29aa579e8a
Lokesh Dhade <115753235+ulambda@users.noreply.github.com>
2024-11-03 10:35:55 +1300

    improved tests

--- src/test/java/com/github/ulambda/core/CharacterTest.java
+++ src/test/java/com/github/ulambda/core/CharacterTest.java
@@ -1,9 +1,10 @@
 package com.github.ulambda.core;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+import com.github.ulambda.core.Character;
 
 public class CharacterTest {
-    @Test public void shouldAnswerWithTrue(){
-        assertTrue( true );
+    @Test public void CreateCharacterWithConstructor(){
+        Character c = new Character(10, 20, 30, Stat.BaseHP, 100);
     }
 }
--- src/test/java/com/github/ulambda/core/WeaponTest.java
+++ src/test/java/com/github/ulambda/core/WeaponTest.java
@@ -3,22 +3,27 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class WeaponTest {
-    /**Weapons.of() can get weapons from db as intended*/
-    @Test public void Test1(){
-        var w = Weapons.of("Ibis Piercer");
+    /**
+    * Weapons.of() Factory Method can create Weapon object from CSV database with a given name
+    */
+    @Test public void ofMethodWeaponObjectFromCSV(){
+        var w = Weapons.of("Ibis Piercer"); //get weapon from database
+        //check values are correct
         assertTrue(w.name().equals("Ibis Piercer"));
         assertTrue(w.baseATK() == 565);
         assertTrue(w.mainStatType().equals(Stat.ATKPercent));
         assertTrue(w.mainStatAmount() == 0.276);
     }
 
-    /**cached pointer equality check*/
-    @Test public void Test2(){
+    /**
+    * Check cached weapons have the same reference
+    */
+    @Test public void ofMethodPointerEquality(){
+        //get the same weapon twice
         var w1 = Weapons.of("Ibis Piercer");
         var w2 = Weapons.of("Ibis Piercer");
-        assertTrue(w1 == w2);
-        assertTrue(w1.toString().equals(w2.toString()));
+        assertTrue(w1 == w2); //check for pointer equality
         var w3 = Weapons.of("Mistsplitter Reforged");
-        assertTrue(w1 != w3);
+        assertTrue(w1 != w3); //check for pointer inequality
     }
 }
